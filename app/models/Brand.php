18dbe03a6daff2522bb5e6d5dbdbb7dbc3421e4c
Oriane-Toque <oriane.toque@gmail.com>
2021-05-25 13:57:39 +0200

    wip models

---
+++ app/models/Brand.php
@@ -0,0 +1,19 @@
+<?php
+
+  // connexion : fCTZ2g8z5
+
+  // DANS LE CADRE D'ACTIVE RECORD
+  // - Une classe = une table en DB = une entité en MCD
+  // - Une propriété de cette classe = un champ de cette table
+  // - Un objet instancié de cette classe = une ligne dans la table (un enregistrement)
+
+  // nom de la classe = nom de la table
+  class Brand {
+
+    // propriétés = lignes de nos tables
+    private $id;
+    private $name;
+    private $footer_order;
+    private $created_at;
+    private $updated_at;
+  }
---
+++ app/models/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+    // nom de la classe = nom de la table
+    class Category {
+
+      // propriétés = lignes de nos tables
+      private $id;
+      private $name;
+      private $subtitle;
+      private $pîcture;
+      private $home_order;
+      private $created_at;
+      private $updated_at;
+    }
