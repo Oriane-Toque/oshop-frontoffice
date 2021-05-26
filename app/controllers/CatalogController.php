430e1242f61593bedadf84e4f2991cb7b7f8b5d9
Oriane-Toque <oriane.toque@gmail.com>
2021-05-26 11:49:15 +0200

    display dynamization products by category

--- app/controllers/CatalogController.php
+++ app/controllers/CatalogController.php
@@ -4,15 +4,23 @@
 
     public function category($routeVarInfos) {
 
+      dump($routeVarInfos);
       // on instancie notre model
       $category = new Category();
       // on appelle ensuite sa méthode find pour récupérer la bonne catégorie
       $category = $category->find($routeVarInfos['id']);
       /* dump($category->getName()); */
 
-      /* $category->findAll(); */
+      // on a besoin de récupérer les produits par catégorie
+      $productModel = new Product();
+      // vu qu'on a récupéré la catégorie précédemment, on peut utiliser notre variable $category et récupérer son id grace à son getter
+      $products = $productModel->findByCategory($category->getId());
 
-      $viewVars = [ 'category' => $category ];
+      // création de clés => valeurs
+      $viewVars = [ 
+        'category' => $category,
+        'products' => $products
+      ];
 
       $this->show('product.list', $viewVars);
 
