0e587e29ad0e446c2d999d03c2c343770cd0ccdd
Oriane-Toque <oriane.toque@gmail.com>
2021-05-27 11:06:18 +0200

    liens category products

--- app/views/product.list.tpl.php
+++ app/views/product.list.tpl.php
@@ -47,17 +47,17 @@
       <?php foreach($viewVars['products'] as $currentProduct) : ?>
       <div class="product col-xl-3 col-lg-4 col-sm-6">
         <div class="product-image">
-          <a href="details.html" class="product-hover-overlay-link">
+          <a href="<?= $router->generate('catalog.product', ['id' => $currentProduct->getId()]) ?>" class="product-hover-overlay-link">
             <img src="<?= $_SERVER['BASE_URI'] .'/'. $currentProduct->getPicture(); ?>" alt="product" class="img-fluid">
           </a>
         </div>
         <div class="product-action-buttons">
           <a href="#" class="btn btn-outline-dark btn-product-left"><i class="fa fa-shopping-cart"></i></a>
-          <a href="detail.html" class="btn btn-dark btn-buy"><i class="fa-search fa"></i><span class="btn-buy-label ml-2">Voir</span></a>
+          <a href="<?= $router->generate('catalog.product', ['id' => $currentProduct->getId()]) ?>" class="btn btn-dark btn-buy"><i class="fa-search fa"></i><span class="btn-buy-label ml-2">Voir</span></a>
         </div>
         <div class="py-2">
           <p class="text-muted text-sm mb-1"><?= $viewVars['category']->getName(); ?></p>
-          <h3 class="h6 text-uppercase mb-1"><a href="detail.html" class="text-dark"><?= $currentProduct->getName(); ?></a></h3><span class="text-muted"><?= $currentProduct->getPrice(); ?></span>
+          <h3 class="h6 text-uppercase mb-1"><a href="<?= $router->generate('catalog.product', ['id' => $currentProduct->getId()]) ?>" class="text-dark"><?= $currentProduct->getName(); ?></a></h3><span class="text-muted"><?= $currentProduct->getPrice(); ?></span>
         </div>
       </div>
       <?php endforeach; ?>
